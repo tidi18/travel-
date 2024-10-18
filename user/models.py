ed01c8852d9ac86e2b464aab7df713743628ec5b
tidi18 <dastan.temirov19@gmail.com>
2024-10-18 14:46:23 +0600

    добавил иконки

--- user/models.py
+++ user/models.py
@@ -7,6 +7,7 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name='Пользователь')
     countries_interest = models.ManyToManyField(Country, blank=True, verbose_name="Интересующие страны")
     post_count = models.IntegerField(default=0, verbose_name='Количество постов')
+    followers = models.ManyToManyField(User, related_name='following', blank=True, verbose_name='Подписчики')
     followers_count = models.IntegerField(default=0, verbose_name='Количество подписчиков')
     is_create = models.BooleanField(default=True, verbose_name='Создавать посты')
     is_blocked = models.BooleanField(default=False, verbose_name='Заблокирован')
